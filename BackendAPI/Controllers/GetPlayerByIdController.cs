bf214fff92de99d5f2852445eabfead8f003952f
ElianRemaggi <elianremaggi@gmail.com>
2022-06-01 21:12:57 -0300

    Add PlayerDataController
    Add PlayerDataController

--- BackendAPI/Controllers/GetPlayerByIdController.cs
+++ BackendAPI/Controllers/GetPlayerByIdController.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Text.Json;
+
 
 namespace BackendAPI.Controllers
 {
---
+++ BackendAPI/Controllers/PlayerDataController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BackendAPI.Controllers
+{
+    [Route("UpdatePlayerData")]
+    [ApiController]
+    public class PlayerDataController : ControllerBase
+    {
+        private static readonly Player[] Players = new[]
+        {
+        new Player("lgarcia",new PlayerData(1,"Luis")),
+            new Player("eremaggi",new PlayerData(5,"Elian")),
+            new Player("yisus",new PlayerData(8,"Jesus")),
+            new Player("seba",new PlayerData(15,"Sebastian"))
+         };
+
+        [HttpPost("id")]
+        public void GetPlayerById(string playerDataSerialized)
+        {
+            Console.WriteLine(JsonConvert.DeserializeObject(playerDataSerialized).ToString());
+
+            PlayerData respuesta = JsonConvert.DeserializeObject<PlayerData>(playerDataSerialized);
+
+            Console.WriteLine("Nombre = " + respuesta.Name + " con una cantidad de victorias de = " + respuesta.WinsAmount);   
+        }
+    }
+}
