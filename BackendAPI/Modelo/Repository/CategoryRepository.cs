ec9b2db5a975c6073def6992533d73bee0331d72
ElianRemaggi <elianremaggi@gmail.com>
2022-06-07 17:36:50 -0300

    Category repository
    Category repository

---
+++ BackendAPI/Modelo/Repository/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace BackendAPI.Modelo.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        List<Category> _categorys = new List<Category>();
+        
+        public CategoryRepository()
+        {
+          _categorys =  LoadCategoryList();
+        }
+        public List<Category> LoadCategoryList()
+        {
+            string path = @"data\categorys.json";
+            using (StreamReader jsonStream = File.OpenText(path))
+            {
+                var json = jsonStream.ReadToEnd();
+
+                Category[] category = JsonConvert.DeserializeObject<Category[]>(json);
+
+                return category.ToList();
+            }
+        }
+        public int CategoryRepositoryLength()
+        {
+            return this._categorys.Count;
+        }
+
+        public Category FindCategoryById(string ID)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
