35926a6603cba437d5b7607cd1e27c919456dc01
ElianRemaggi <elianremaggi@gmail.com>
2022-06-19 14:26:40 -0300

    ServiceProvider Implemented
    ServiceProvider Implemented

---
+++ APITests/Service/ServiceProviderTests.cs
@@ -0,0 +1,33 @@
+﻿using BackendAPI.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITests.Service
+
+{
+    public class ServiceProviderTests
+    {
+        Player _testPlayer;
+        [SetUp]
+        public void Setup()
+        {
+            _testPlayer = new Player("1", new PlayerData(3, "Luis", new List<PowerUp>()));
+        }
+
+        [Test]
+        public void Service_Provider_Should_Be_Able_To_Return_A_Player_When_Find_Player_By_Id()
+        {
+            //Arrange
+            string id = "eremaggi";
+            ServiceProvider serviceProvider = new ServiceProvider();
+            //Act
+            serviceProvider.GetPlayerById(id);
+            //Assert
+            Assert.IsInstanceOf(typeof(Player), serviceProvider.GetPlayerById(id));
+        }
+    }
+}
---
+++ BackendAPI/Service/ServiceProvider.cs
@@ -0,0 +1,16 @@
+﻿using BackendAPI.Modelo.Repository;
+
+namespace BackendAPI.Service
+{
+    public class ServiceProvider
+    {
+
+        public Player GetPlayerById(string id)
+        {
+            IPlayerRepository playerRepository;
+            playerRepository = new PlayerRepository();
+            return playerRepository.FindPlayerById(id);
+        }
+
+    }
+}
