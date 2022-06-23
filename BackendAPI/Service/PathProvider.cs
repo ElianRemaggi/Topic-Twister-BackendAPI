7194698f37ea5943979a092c2201248a8844e819
ElianRemaggi <elianremaggi@gmail.com>
2022-06-23 20:59:49 -0300

    PathProvider Letter
    PathProvider Letter

---
+++ BackendAPI/Service/PathProvider.cs
@@ -0,0 +1,13 @@
+﻿using BackendAPI.Modelo.Repository;
+using BackendAPI.Modelo.UseCases;
+
+namespace BackendAPI.Service
+{
+    public class PathProvider
+    {
+        public static string GetLetterJsonPath()
+        {
+            return @"data\letters.json";
+        }       
+    }
+}
